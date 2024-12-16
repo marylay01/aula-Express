db62da360a9db6a2b06c26024c05dc481a083e19
Reh <robinhoitaimcoelho@gmail.com>
2024-12-16 08:07:36 -0300

    atualizacoes

---
+++ app.js
@@ -0,0 +1,22 @@
+import Express from 'express'
+import logger from './middlewares/logger.js'
+import userRouter from './router/users.js'
+
+const app = Express()
+app.use(Express.json())
+
+app.use(logger)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.use(userRouter)
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('app online na porta 3000')
+});
+
---
+++ router/users.js
@@ -0,0 +1,27 @@
+import {PrismaClient} from '@prisma/client'
+import {Router} from 'express'
+
+constuserRouter = Router()
+const prisma = new PrismaClient()
+
+userRouter.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany({});
+    res.json(users)
+})
+
+userRouter.post('/users', async (req, res) => {
+    const user = req.body
+    const userSaved = await prisma.user.create(
+      {
+        data: user
+      }
+    )
+    res.send(userSaved)
+  })
+
+  export default userRouter
+
+
+
+
+
